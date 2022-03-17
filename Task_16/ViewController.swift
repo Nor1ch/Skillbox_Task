94fe8a7f478b886f8cfafe37daf2023e105c4c2a
Nor1ch <iliacherepanin@gmail.com>
2022-03-17 20:10:07 +0300

    commit 1

---
+++ Task_16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task_16
+//
+//  Created by mac on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
